6f9ef5e566cc5f1c7c726dc1fcbd0f232a497eca
Rohama Salfi <rohxama@gmail.com>
2024-10-01 17:44:14 -0700

    All done

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz App-Home</title>
+    <title>MCQZONE-Home</title>
     <!-- Remix Icons -->
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
     <!-- Font Family -->
--- quiz.php
+++ quiz.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz App</title>
+    <title>MCQZONE-Quiz</title>
 
     <link rel="stylesheet" href="./assets/style/style.css">
 
